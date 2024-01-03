9d31305da8b617993462fc7b7609b11ca8d0f219
kidmpukane <lwandisozwane91@gmail.com>
2024-01-03 16:25:15 +0200

    crud-complete

--- kaire_skincare/recommendations/views.py
+++ kaire_skincare/recommendations/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework import status
+from rest_framework.response import Response
+from .models import Products
+from .serializers import ProductSerializer
 
-# Create your views here.
+
+# .......................................GET ALL PRODUCT DATA.......................................
+
+def get_all_data(queryset, serializer_class):
+    data = queryset.objects.all()
+    serializer = serializer_class(data, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def get_all_merchant_data(request):
+    return get_all_data(Products, ProductSerializer)
+
+
+# .......................................GET PRODUCT BY ID.......................................
+
+def get_product(request, id, profile_model, serializer_class):
+    try:
+        user_profile_data = profile_model.objects.get(pk=id)
+    except profile_model.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = serializer_class(user_profile_data)
+        return Response(serializer.data)
+
+
+@api_view(['GET'])
+def get_customer_profile(request, id):
+    return get_product(request, id, Products, ProductSerializer)
--- kaire_skincare/user_profiles/views.py
+++ kaire_skincare/user_profiles/views.py
@@ -17,3 +17,22 @@ def register_user(serializer_class, request):
 @api_view(['POST'])
 def user_registration(request):
     return register_user(UserInfoSerializer, request)
+
+# .......................................EDIT USER DATA.......................................
+
+
+@api_view(['PUT'])
+def update_user_data(request, id, queryset, serializer_class):
+    user_profiles_info_update = queryset.objects.get(pk=id)
+    serializer = serializer_class(
+        instance=user_profiles_info_update, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(['PUT'])
+def update_user_data(request, id):
+    return update_user_data(request, id, UserInfo, UserInfoSerializer)
