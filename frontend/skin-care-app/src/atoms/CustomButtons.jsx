19038c8869098c17424ad8a08b75e76c98829c49
kidmpukane <lwandisozwane91@gmail.com>
2024-01-17 10:58:26 +0200

    toggle-with-bug

--- frontend/skin-care-app/src/atoms/CustomButtons.jsx
+++ frontend/skin-care-app/src/atoms/CustomButtons.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./atomicStyles.css";
 
 const RoundButton = (props) => {
@@ -14,18 +14,25 @@ const RoundButton = (props) => {
     </button>
   );
 };
-
 const SquareButton = (props) => {
+  const [bttnColour, setBttnColour] = useState("square-button");
+
+  const changeColour = () => {
+    setBttnColour((prevColor) =>
+      prevColor === "square-button" ? "green-button" : "square-button"
+    );
+  };
+
   return (
     <button
-      className={
-        props.className ? `${props.className} square-button` : "square-button"
-      }
-      onClick={props.onClick}
+      className={bttnColour}
+      onClick={() => {
+        props.onClick();
+        changeColour();
+      }}
     >
       {props.squareButtonText}
     </button>
   );
 };
-
 export { SquareButton, RoundButton };
